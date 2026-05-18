1ecd3465c4b33f548d74cf9e585989671dd75b36
Thrisha sri <thrishasri5112004@gmail.com>
2026-05-18 15:12:06 +0530

    Add files via upload

---
+++ practice/src/Practice/ControlFl.java
@@ -0,0 +1,21 @@
+package Practice;
+
+public class ControlFl {
+
+	public static void main(String[] args) {
+		
+				int a =10;
+				int b =30;
+				int c =40;
+				if(a>b) {
+					System.out.println("a is greater");
+				}
+				if (a==b) {
+					System.out.println("a is equal to b");
+				}
+
+			}
+
+	}
+
+
---
+++ practice/src/Practice/ControlFlow.java
@@ -0,0 +1,19 @@
+package Practice;
+import java.util.*;
+
+public class ControlFlow {
+
+	public static void main(String[] args) {
+		int a =10;
+		int b =30;
+		int c =40;
+		if(a>b) {
+			System.out.println("a is greater");
+		}
+		if (a==b) {
+			System.out.println("a is equal to b");
+		}// TODO Auto-generated method stub
+
+	}
+
+}
